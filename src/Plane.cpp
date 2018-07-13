6af72081e4f6279abf88532e1964035e2352548b
Brendan <brendan_1391@yahoo.com>
2018-07-13 15:36:28 -0700

    Planes files added

---
+++ src/Plane.cpp
@@ -0,0 +1,34 @@
+#include "Plane.hpp"
+
+Plane::Plane(glm::dvec3 center, glm::dvec3 normal) :
+_center(center),
+_normal(normal)
+{}
+
+double Plane::Intersection(const Ray& ray) const
+{
+	double		denom;
+	glm::dvec3	subtract;
+	double		dist;
+
+	denom = glm::dot(_normal, ray.direction);
+	if (glm::abs(denom) < 0.0001)
+		return INFINITY;
+	subtract = ray.origin - _center;
+	dist = glm::dot(subtract, ray.direction) / denom;
+	return (dist > 0 ? dist : INFINITY);
+}
+
+RayResult Plane::MakeRayResult(double distance, const Ray& ray) const
+{
+	RayResult out;
+
+	out.position = ray.origin + ray.direction * distance;
+	out.normal = glm::normalize(out.position - _center);
+	out.color = glm::dvec3(1, 0, 0);
+	out.diffuse = 1;
+	out.reflect = 0;
+	out.refract = 0;
+	out.refractiveIndex = 1;
+	return out;
+}
