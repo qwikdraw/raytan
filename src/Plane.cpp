3f71af666a3dace6da681031ff60a5a918a7523d
theo-walton <theodore_walton@hotmail.com>
2018-07-16 16:09:16 -0700

    ok

--- src/Plane.cpp
+++ src/Plane.cpp
@@ -1,5 +1,21 @@
 #include "Plane.hpp"
 
+std::vector<std::pair<double, IObject*>> Plane::findDistances(const Ray& ray) const
+{
+	double dist = glm::dot(ray.direction, direction);
+	if (dist == 0)
+		return std::vector<std::pair<double, IObject*>>();
+	dist = glm::dot(center - ray.origin, direction) / dist;
+
+	std::pair<double, IObject*> p;
+	p.first = dist;
+	p.second = (IObject*)this;
+	std::vector<std::pair<double, IObject*>> out;
+	out.push_back(p);
+	
+	return out;
+}
+
 glm::dvec3 Plane::findNormal(const glm::dvec3& intersection, const Ray& ray) const
 {
 		return direction;
@@ -10,15 +26,4 @@ glm::dvec2 Plane::uvMap(const glm::dvec3&, const glm::dvec3&) const
 {
 	// ill implement later
 	return glm::dvec2(0);
-}
-
-double Plane::Intersection(const Ray& ray) const
-{
-	double dist = glm::dot(ray.direction, direction);
-	if (dist == 0)
-		return INFINITY;
-	dist = glm::dot(center - ray.origin, direction) / dist;
-	if (dist < 0)
-		return INFINITY;
-	return dist;
 }
