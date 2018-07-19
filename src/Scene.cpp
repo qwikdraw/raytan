dfab38d790702598322783f6acc42ca72d7ce456
Logan James Kaser <logankaser@fyrn.io>
2018-07-19 13:08:18 -0700

    Fix merge mistake

--- src/Scene.cpp
+++ src/Scene.cpp
@@ -6,7 +6,7 @@
 /*   By: bpierce <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/11 16:08:11 by bpierce           #+#    #+#             */
-/*   Updated: 2018/07/19 13:02:54 by lkaser           ###   ########.fr       */
+/*   Updated: 2018/07/19 13:06:44 by lkaser           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,91 +15,6 @@
 Scene::Scene(void)
 {
 	_ambient = glm::dvec3(0.0);
-
-	Sphere *s1 = new Sphere;
-	s1->center = glm::dvec3(1, 0, 0.1);
-	s1->direction = glm::dvec3(0, 0, 1);
-	
-	s1->radius = 0.2;
-	s1->color = glm::dvec3(1, 1, 1);
-	s1->refractiveIndex = 1.6;
-	s1->diffuse = 0.1;
-	s1->reflect = 0;
-	s1->refract = 0.9;
-	s1->color = glm::dvec3(0.9, 0.5, 0.8);
-	
-	s1->colorSampler = new Sampler("assets/image.png");
-	s1->materialSampler = nullptr;//new Sampler("mat.png");
-	s1->normalSampler = nullptr;
-
-	Plane *p2 = new Plane;
-        p2->center = glm::dvec3(1, 0, 0.1);
-        p2->direction = glm::normalize(glm::dvec3(1, 0, 1));
-
-        p2->refractiveIndex = 1.6;
-        p2->diffuse = 0.1;
-        p2->reflect = 0;
-        p2->refract = 0.9;
-        p2->color = glm::dvec3(0.9, 0.5, 0.8);
-
-        p2->colorSampler = nullptr;
-        p2->materialSampler = nullptr;
-        p2->normalSampler = nullptr;
-
-
-	Plane *p3 = new Plane;
-        p3->center = glm::dvec3(-1, 0, 0.1);
-        p3->direction = glm::normalize(glm::dvec3(1, 0, 1));
-
-        p3->refractiveIndex = 1.0;
-        p3->diffuse = 0.2;
-        p3->reflect = 0.8;
-        p3->refract = 0;
-        p3->color = glm::dvec3(0.2, 1, 0.2);
-
-        p3->colorSampler = nullptr;
-        p3->materialSampler = nullptr;
-        p3->normalSampler = nullptr;
-		_objects.push_back(p3);
-
-//	Subtraction *sub = new Subtraction(s1, p2);
-	
-//	_objects.push_back(sub);
-	_objects.push_back(s1);
-
-	Plane *p1 = new Plane;
-	p1->center = glm::dvec3(2, 0, 0);
-	p1->direction = glm::dvec3(1, 0, 0);
-
-	p1->refractiveIndex = 2;
-	p1->diffuse = 1;
-	p1->reflect = 0;
-	p1->refract = 0;
-	p1->color = glm::dvec3(1, 1, 0.5);
-
-	p1->colorSampler = nullptr;
-	p1->materialSampler = nullptr;
-	p1->normalSampler = nullptr;
-
-	_objects.push_back(p1);
-
-	Cylinder *c1 = new Cylinder;
-	c1->center = glm::dvec3(2, 0.3, 0.3);
-	c1->radius = 0.2;
-	c1->vector = glm::normalize(glm::dvec3(0.1, 0.3, 0.7));
-	c1->color = glm::dvec3(1, 1, 1);
-	c1->refractiveIndex = 2;
-	c1->diffuse = 1;
-	c1->reflect = 0;
-	c1->refract = 0;
-	c1->colorSampler = nullptr;
-	c1->materialSampler = nullptr;
-	c1->normalSampler = nullptr;
-
-	_objects.push_back(c1);
-
-	_lights.push_back((Light){{0, -0.5, 0.5}, {4, 4, 4}});
-
 }
 
 Scene::~Scene(void)
