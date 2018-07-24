f1c24a88daae7085722d3f5eb2eb5cbb959690e5
theo-walton <theodore_walton@hotmail.com>
2018-07-24 14:19:41 -0700

    Addition object logic is done, now for testing

---
+++ src/Addition.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include "Raytan.hpp"
+#include "IObject.hpp"
+#include "Subtraction.hpp"
+
+class	Addition : public IObject
+{
+	IObject const* _shape1;
+	IObject const* _shape2;
+
+	bool isFacing(const Intersect& i, const Ray& ray) const;
+	std::vector<Edge> generateEdges(const std::vector<Intersect>& s1,
+					const std::vector<Intersect>& s2,
+					const Ray& ray) const;
+	std::vector<Intersect> getIntersectionsFrom(const IObject*, const Ray&) const;
+	std::vector<Intersect> findIntersections(const Ray&) const;
+
+	// will have dummy implementation
+	std::vector<double> findDistances(const Ray&) const;
+public:
+	Addition(IObject* shape1, IObject* shape2);
+
+	bool IsPrimitive(void) const;
+	Intersect Intersection(const Ray&) const;
+};
