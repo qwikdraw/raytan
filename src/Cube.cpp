21bd34629d61c7943101f51041f07f7783499cd4
Brendan <brendan_1391@yahoo.com>
2018-07-24 21:50:51 -0700

    Fixed Cube normals and added the Cube UV map

--- src/Cube.cpp
+++ src/Cube.cpp
@@ -26,42 +26,46 @@ glm::dvec2		Cube::solveQuadratic(double a, double b, double c) const
 
 glm::dvec3		Cube::findNormal(const glm::dvec3& intersection) const
 {
-	// glm::dvec3 absI = glm::abs(intersection);
-	// std::cout << absI.x << std::endl;
+	double	bias = 0.00001;
+	double	distX = intersection.x / (size.x / 2.0);
+	double	distY = intersection.y / (size.y / 2.0);
+	double	distZ = intersection.z / (size.z / 2.0);
 
-	// if (absI.x > absI.y && absI.x > absI.z && intersection.x < 0)
-	// 	return glm::dvec3(-1, 0, 0);
-	// if (absI.x > absI.y && absI.x > absI.z && intersection.x > 0)
-	// 	return glm::dvec3(1, 0, 0);
-	// if (absI.y > absI.x && absI.y > absI.z && intersection.y < 0)
-	// 	return glm::dvec3(0, -1, 0);
-	// if (absI.y > absI.x && absI.y > absI.z && intersection.y > 0)
-	// 	return glm::dvec3(0, 1, 0);
-	// if (intersection.z < 0)
-	// 	return glm::dvec3(0, 0, -1);
-	// return glm::dvec3(0, 0, 1);
+	// Return X-facing normal
+	if (distX > 0 && distX > 1 - bias)
+		return glm::dvec3(1, 0, 0);
+	if (distX < 0 && distX < bias - 1)
+		return glm::dvec3(-1, 0, 0);
 
+	// Return Y-facing normal
+	if (distY > 0 && distY > 1 - bias)
+		return glm::dvec3(0, 1, 0);
+	if (distY < 0 && distY < bias - 1)
+		return glm::dvec3(0, -1, 0);
 
+	// Return Z-facing normal
+	if (distZ > 0 && distZ > 1 - bias)
+		return glm::dvec3(0, 0, 1);
+	if (distZ < 0 && distZ < bias - 1)
+		return glm::dvec3(0, 0, -1);
 
-	
-	// Getting the first vector from center to intersection point
-	glm::dvec3 pVec = intersection;
-
-	// Grab the 'divisor' vectors
-	glm::dvec3 divisor = glm::abs(size) / 2.0;
-
-	// Calculate the normal vector
-	glm::dvec3 normal =  glm::round(pVec);
-	normal.x = (int)(pVec.x / divisor.x * 1.0001);
-	normal.y = (int)(pVec.y / divisor.y * 1.0001);
-	normal.z = (int)(pVec.z / divisor.z * 1.0001);
-
-	return glm::normalize(normal);
-
+	// Return a normal based on the intersection to center (0, 0, 0) itself
+	return glm::normalize(intersection);
 }
 
 glm::dvec2		Cube::uvMap(const glm::dvec3& intersection, const glm::dvec3& normal) const
 {
+	double	distX = (intersection.x / (size.x)) + 0.5;
+	double	distY = (intersection.y / (size.y)) + 0.5;
+	double	distZ = (intersection.z / (size.z)) + 0.5;
+
+	// X-facing normal, product of changing y and z
+	if (normal.x == 1 || normal.x == -1)
+		return glm::dvec2(distY, distZ);
+	if (normal.y == 1 || normal.y == -1)
+		return glm::dvec2(distX, distZ);
+	if (normal.z == 1 || normal.z == -1)
+		return glm::dvec2(distX, distY);
 	return glm::dvec2(0.5, 0.5); // Will  update later
 }
 
