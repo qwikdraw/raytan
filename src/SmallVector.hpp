727e40c932106deb15a3127bfca8518dde70d927
theo-walton <theodore_walton@hotmail.com>
2018-07-28 18:48:26 -0700

    adding small vector

---
+++ src/SmallVector.hpp
@@ -0,0 +1,52 @@
+#pragma once
+
+template <class T>
+class	SmallVector
+{
+	constexpr size_t _stackSize = 6;
+	T _stack[_stackSize];
+	std::vector<T>* _backup;
+	size_t _size;
+public:
+	SmallVector()
+	{
+		_backup = nullptr;
+		_size = 0;
+	}
+	~SmallVector()
+	{
+		delete _backup;
+	}
+
+	void	push_back(const T& item)
+	{
+		if (_size < _stackSize)
+		{
+			_stack[_size] = item;
+			_size++;
+		}
+		else
+		{
+			_backup = new std::vector<T>;
+			_backup->push_back(item);
+			_size++;
+		}
+	}
+	size_t	size() const
+	{
+		return _size;
+	}
+	
+	T&	operator[](size_t index)
+	{
+		if (index < _stackSize)
+			return _stack[index];
+		return _backup[index - _size]
+	}
+	const T&	operator[](size_t index) const
+	{
+		if (index < _stackSize)
+			return _stack[index];
+		return _backup[index - _size];
+	}	
+};
